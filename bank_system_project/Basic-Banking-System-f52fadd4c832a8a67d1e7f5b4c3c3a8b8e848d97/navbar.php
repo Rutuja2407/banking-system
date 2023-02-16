18e93febde81af4ded0f48927b4392a523242708
Rutuja2407 <108170905+Rutuja2407@users.noreply.github.com>
2023-02-16 19:44:37 +0530

    Add files via upload

---
+++ bank_system_project/Basic-Banking-System-f52fadd4c832a8a67d1e7f5b4c3c3a8b8e848d97/navbar.php
@@ -0,0 +1,17 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-light" style="height: 43px;margin:0;">
+      <a class="navbar-brand" href="index.php">The GRIP Bank</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li>
+                <a href="index.php">HOME</a>
+              </li>
+
+              <li>
+                <a href="transactionhistory.php">HISTORY</a>
+              </li>
+          </div>
+       </nav>
